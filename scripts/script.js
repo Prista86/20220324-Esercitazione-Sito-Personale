596e71313319da3c6db324feb6d1b2b22636e095
LukePC <LukePC@DESKTOP-SMI2KCQ>
2022-03-26 01:05:06 +0100

    finito

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-class Partecipanti {        //creazione classe 
+class Partecipanti { //creazione classe 
     constructor(id, nome, cognome, datadinascita, email, immagine) {
-        this.id=id;
+        this.id = id;
         this.nome = nome;
         this.cognome = cognome;
         this.datadinascita = datadinascita;
@@ -9,49 +9,65 @@ class Partecipanti {        //creazione classe
     }
 }
 var indice = 0;
-const p1 = new Partecipanti(1,'Luca','Rossi','01.01.1981','aaa@gmail.com',"image/corgi1.jpg");
-const p2 = new Partecipanti(2,'Mario','Verdi','02.02.1982','bbb@gmail.com',"image/corgi2.jpg");
-const p3 = new Partecipanti(3,'Giacomo','Bianchi','03.03.1983','ccc@gmail.com',"image/corgi3.jpg");
-const p4 = new Partecipanti(4,'Francesco','Gialli','04.04.1984','ddd@gmail.com',"image/corgi4.jpg");
-const p5 = new Partecipanti(5,'Paolo','Neri','05.05.1985','eee@gmail.com',"image/corgi5.jpg");
-const ListaPartecipanti = [p1, p2, p3, p4,p5];
+const p1 = new Partecipanti(1, 'Luca', 'Rossi', '01.01.1981', 'aaa@gmail.com', "image/corgi1.jpg");
+const p2 = new Partecipanti(2, 'Mario', 'Verdi', '02.02.1982', 'bbb@gmail.com', "image/corgi2.jpg");
+const p3 = new Partecipanti(3, 'Giacomo', 'Bianchi', '03.03.1983', 'ccc@gmail.com', "image/corgi3.jpg");
+const p4 = new Partecipanti(4, 'Francesco', 'Gialli', '04.04.1984', 'ddd@gmail.com', "image/corgi4.jpg");
+const p5 = new Partecipanti(5, 'Paolo', 'Neri', '05.05.1985', 'eee@gmail.com', "image/corgi5.jpg");
+const ListaPartecipanti = [p1, p2, p3, p4, p5];
 
 StampaListaPartecipanti();
 
-const btnP = document.getElementById('btnP');  //gestione bottone Precedente con incremento indice
-btnP.onclick = function () {
+const btnP = document.getElementById('btnP'); //gestione bottone Precedente con incremento indice
+btnP.onclick = function() {
     indice--;
-        if(indice == -1){
-            indice=3;
-        }
+    if (indice == -1) {
+        indice = 3;
+    }
     console.log(indice);
     StampaListaPartecipanti();
 }
-const btnS = document.getElementById('btnS');   //gestione bottone Precedente con incremento indicedddfd
-btnS.onclick = function () {
+const btnS = document.getElementById('btnS'); //gestione bottone Precedente con incremento indicedddfd
+btnS.onclick = function() {
     indice++;
-        if(indice == 4){
-            indice=0;
-        }
+    if (indice == 4) {
+        indice = 0;
+    }
     console.log(indice);
     StampaListaPartecipanti();
 }
-function StampaListaPartecipanti() {  //completa la tabella con le info della classee
+
+function StampaListaPartecipanti() { //completa la tabella con le info della classee
     const btn1 = document.getElementById('btn1');
-    btn1.innerText= ListaPartecipanti[indice].nome;
+    btn1.innerText = ListaPartecipanti[indice].nome;
     const div = document.getElementById('lista');
-    div.innerHTML =`
+    div.innerHTML = `
     <p>Nome : ${ListaPartecipanti[indice].nome} </p>
     <p>ID: ${ListaPartecipanti[indice].id}</p>
     <p>Cognome: ${ListaPartecipanti[indice].cognome}</p>
     <p>Data di Nascita: ${ListaPartecipanti[indice].datadinascita}</p>
-    <p>Email: ${ListaPartecipanti[indice].email}</p>    
-    <p>Foto: </p><span><img id="img1" src="${ListaPartecipanti[indice].immagine}" class="rounded mx-auto center" alt="Torno Subito..."></span>
-    `    
+    <p>Email: ${ListaPartecipanti[indice].email}</p>  
+    <div class="card">
+        <div class="accordionCol card-header">
+            <p class="collapsed btn" data-bs-toggle="collapse" href="#collapseOne">
+                Clicca per vedere la foto!!!
+            </p>
+        </div>
+        <div id="collapseOne" class="accordionCol collapse" data-bs-parent="#accordion">
+            <div class="card-body">
+                <p>Foto: </p><img id="img1" src="${ListaPartecipanti[indice].immagine}" class="rounded mx-auto center" alt="Torno Subito...">
+            </div>
+        </div>
+    </div>    
+    `
 }
-$(document).ready(function(){
-    $('#btn1').click(function(){
-      $('#lista').slideToggle("slow");
-      
+$(document).ready(function() {
+    $('#lista').hide();
+});
+$(document).ready(function() {
+    $('#btn1').click(function() {
+        $('#lista').slideToggle(800);
+        (this).hide();
+
     });
 });
